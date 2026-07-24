afaf17a51947ef1a82bf77bfcef27e2b03ff0060
mgsaichiru1212 <saichiru1212mg@gmail.com>
2026-07-24 17:23:27 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-
 engine = create_engine('mysql+mysqlconnector://root:2266@localhost:3306/retail')
 
-file_path = r'c:\Users\SAI CHIRU\Videos\Retail 40\RetailSales_CustomerProjectAnalysiss.csv'
-
+file_path = r'c:\Users\SAI CHIRU\Videos\Netflix_400000.csv'
 
 df = pd.read_csv(file_path)
 
-
-table_name = 'RetailSales_CustomerProjectAnalysis'
+table_name = 'netflix'
 df.to_sql(table_name, con=engine, if_exists='append', index=False, chunksize=1000)
 
-print("డేటా విజయవంతంగా మైఎస్క్యుఎల్ టేబుల్‌లోకి ఇన్సర్ట్ అయింది!")
+print("sucessfully uploaded!")
